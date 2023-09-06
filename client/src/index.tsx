530ae615ba8be3ecad31b79a6c57f1d22bf9be22
ghyorka <ghyorkagospel@gmail.com>
2023-09-06 23:59:18 +0200

    fix(index) Fix issue with Tailwind CSS not working

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>
 );
 
