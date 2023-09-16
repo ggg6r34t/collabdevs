a366a995f8e687a6468b8317689efd0be73bc18f
ghyorka <ghyorkagospel@gmail.com>
2023-09-16 10:26:37 +0200

    chore(upload) Move multer configuration to user routes

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
+import multer from "multer";
 
 import {
   createUserController,
@@ -17,6 +18,32 @@ import {
 import adminCheck from "../middlewares/adminCheck";
 
 const router = Router();
+
+// multer configuration for file name and upload storage location
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const userId = req.params.id;
+
+    // files name after the user's id
+    const userDirectory = `./uploads/${userId}/`;
+
+    callback(null, userDirectory);
+  },
+  filename: (req, file, callback) => {
+    // original filename with a timestamp prefix
+    const timestamp = new Date().toISOString();
+    const originalFilename = file.originalname;
+
+    // timestamp and original filename to create a unique filename
+    const uniqueFilename = `${timestamp}-${originalFilename}`;
+
+    // callback function with the filename
+    callback(null, uniqueFilename);
+  },
+});
+
+// multer instance with the configured storage
+const upload = multer({ storage });
 
 //get: register user
 router.post("/register", createUserController);
@@ -49,6 +76,7 @@ router.put(
 // post: user upload avatar
 router.post(
   "/:id/upload-avatar",
+  upload.single("avatar"),
   passport.authenticate("jwt", { session: false }),
   uploadAvatarController
 );
@@ -56,6 +84,7 @@ router.post(
 // post: user upload banner
 router.post(
   "/:id/upload-banner",
+  upload.single("banner"),
   passport.authenticate("jwt", { session: false }),
   uploadBannerController
 );
