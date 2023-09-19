b5a825d3b29d2b7f96d68dedc9eddea22a2e6fd9
ghyorka <ghyorkagospel@gmail.com>
2023-09-19 10:51:02 +0200

    feat(trendingprojects) Add animate fire asset to trending projects

--- client/src/components/trending/TrendingProjects.tsx
+++ client/src/components/trending/TrendingProjects.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import TrendingProjectItem from "./TrendingProjectItem";
 import { AppDispatch, RootState } from "../../redux/store";
 import { fetchPostData } from "../../redux/thunk/posts";
-import TrendingProjectItem from "./TrendingProjectItem";
+import fire from "../../assets/fire.gif";
 
 function TrendingProjects() {
   const posts = useSelector((state: RootState) => state.posts.posts);
@@ -23,7 +24,14 @@ function TrendingProjects() {
   return (
     <div className=" flex flex-col items-start justify-start p-4 ">
       <div className="w-[280px] h-[400px] border border-gray-400 rounded shadow p-4">
-        <h3 className="text-xl font-semibold mb-4">Trending Projects 🔥🔥🔥</h3>
+        <h3 className="text-xl font-semibold mb-4 flex items-center">
+          Trending Projects
+          <span className="flex flex-row ml-2">
+            <img className="w-6 h-6" src={fire} alt="fire" />
+            <img className="w-6 h-6" src={fire} alt="fire" />
+            <img className="w-6 h-6" src={fire} alt="fire" />
+          </span>
+        </h3>
         <div className="flex flex-col space-y-2">
           {topPosts.map((post) => (
             <div key={post._id}>
