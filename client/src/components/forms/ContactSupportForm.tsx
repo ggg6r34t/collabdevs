0c8648ff2ed279ca8ec3a30e8c092bfc43a6acf3
ghyorka <ghyorkagospel@gmail.com>
2023-09-22 00:01:57 +0200

    feat(support) Add support page component

---
+++ client/src/components/forms/ContactSupportForm.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+
+function ContactSupportForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [feedbackType, setFeedbackType] = useState("Bug");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mb-6 text-center">
+        Contact Support
+      </h1>
+
+      {submitted ? (
+        <div className="bg-green-100 text-green-700 p-4 rounded-md mb-4 text-center">
+          Thank you for reaching out! We'll get back to you shortly.
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-sm font-medium mb-2">
+                Your Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full px-4 py-2 border rounded-md"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium mb-2">
+                Your Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-2 border rounded-md"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="subject"
+                className="block text-sm font-medium mb-2"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="w-full px-4 py-2 border rounded-md"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium mb-2"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                className="w-full px-4 py-2 border rounded-md"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                rows={4}
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="feedbackType"
+                className="block text-sm font-medium mb-2"
+              >
+                Type of Feedback
+              </label>
+              <select
+                id="feedbackType"
+                className="w-full px-4 py-2 border rounded-md"
+                value={feedbackType}
+                onChange={(e) => setFeedbackType(e.target.value)}
+                required
+              >
+                <option value="Bug">Bug Report</option>
+                <option value="FeatureRequest">Feature Request</option>
+                <option value="General">General Inquiry</option>
+              </select>
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="px-4 py-2 bg-[#010536] text-white rounded-md focus:outline-none focus:ring focus:border-blue-400"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ContactSupportForm;
