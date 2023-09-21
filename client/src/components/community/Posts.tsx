baab6a32292745ff665b2e189a03adda9ccd9fd6
ghyorka <ghyorkagospel@gmail.com>
2023-09-21 23:58:41 +0200

    style(post) Change the styling for the post form

--- client/src/components/community/Posts.tsx
+++ client/src/components/community/Posts.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-regular-svg-icons";
@@ -10,13 +10,13 @@ import "react-quill/dist/quill.bubble.css";
 import { AppDispatch, RootState } from "../../redux/store";
 import { fetchPostData } from "../../redux/thunk/posts";
 import Post from "./PostItem";
-import TrendingProjects from "../trending/TrendingProjects";
 
 function Posts() {
   const posts = useSelector((state: RootState) => state.posts.posts);
 
-  const fetchDispatch = useDispatch<AppDispatch>();
+  const location = useLocation();
 
+  const fetchDispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,13 +28,19 @@ function Posts() {
   }
 
   return (
-    <div className=" bg-white p-4 mb-4  flex flex-row items-start justify-center">
-      <div className=" bg-white p-4 mb-4  flex flex-col items-center justify-center">
-        <div className="w-640 h-4 p-2 rounded shadow flex flex-row items-center justify-center border border-gray-400">
-          <div className="flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full mr-4"></div>
+    <div className="col-span-2">
+      {location.pathname === "/" ? null : (
+        <div className="w-full bg-white h-4 p-2 mb-4 rounded-lg shadow flex flex-row items-center justify-center">
+          <div className="flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full mr-4">
+            <img
+              src="https://e7.pngegg.com/pngimages/973/940/png-clipart-laptop-computer-icons-user-programmer-laptop-electronics-computer-thumbnail.png"
+              alt="User Avatar"
+              className="flex-shrink-0 w-10 h-10 bg-gray-300 rounded-full mr-4"
+            />
+          </div>
           <div onClick={handleQuillClick}>
             <ReactQuill
-              className="my-custom-quill w-[30rem] h-[3rem] border rounded p-2 focus:outline-none focus:border-blue-500"
+              className="my-custom-quill w-[40rem] h-[3rem] border rounded p-2 focus:outline-none focus:border-blue-500"
               theme="bubble"
               scrollingContainer="null"
               placeholder="What are you working on?"
@@ -47,15 +53,16 @@ function Posts() {
             <FontAwesomeIcon icon={faLink} className="w-40 h-40" />
           </button>
         </div>
-        <div>
-          {posts.map((post) => (
-            <div key={post._id} className="mb-4">
-              <Post post={post} />
-            </div>
-          ))}
-        </div>
+      )}
+
+      {/* filtered and sorted posts */}
+      <div>
+        {posts.map((post) => (
+          <div key={post._id} className="bg-white p-4 rounded-lg shadow mb-4">
+            <Post post={post} />
+          </div>
+        ))}
       </div>
-      <TrendingProjects />
     </div>
   );
 }
--- client/src/components/forms/PostForm.tsx
+++ client/src/components/forms/PostForm.tsx
@@ -9,6 +9,7 @@ import DOMPurify from "dompurify";
 import { Post } from "../../type/types";
 import { RootState } from "../../redux/store";
 import { BASE_URL } from "../../api/api";
+import TrendingProjects from "../trending/TrendingProjects";
 
 function PostForm() {
   const userInformation = useSelector(
@@ -59,7 +60,7 @@ function PostForm() {
 
   return (
     <div className="h-screen bg-white p-4 py-40 flex flex-row items-start justify-center">
-      <div className="w-[738px] flex flex-col items-center justify-center border border-gray-400 rounded shadow p-4">
+      <div className="w-[738px] flex flex-col items-center justify-center rounded shadow p-4">
         <input
           type="text"
           className="w-[708px] h-[40px] mb-4 px-2 border rounded focus:outline-none focus:border-blue-500"
@@ -87,7 +88,7 @@ function PostForm() {
         />
         <div className="w-[724px] h-[45px] flex items-center justify-end p-1 mt-4 ">
           <button
-            className="h-[35px] py-1 px-2 text-blue-500 border-2 rounded-[12px] hover:bg-blue-50 focus:outline-none "
+            className="px-4 py-2 bg-[#010536] text-white rounded-md focus:outline-none focus:ring focus:border-blue-400"
             onClick={() => {
               createPost(postData, userInformation?.token);
             }}
@@ -96,32 +97,8 @@ function PostForm() {
           </button>
         </div>
       </div>
-      <div className=" flex flex-col items-start justify-start ml-4 ">
-        <div className="w-[273px] h-[275px] border border-gray-400 rounded shadow">
-          <div className="p-4">
-            <h3 className="text-xl font-semibold ">Hot topics 🔥🔥🔥</h3>
-          </div>
-          <div className="flex flex-col items-start justify-start p-1">
-            <div className="p-3 pt-2 pb-0">
-              <p className="text-gray-600">
-                Web3 and the Future of the Internet
-              </p>
-            </div>
-            <div className="pl-3">
-              <p>@evans</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-start justify-start p-1">
-            <div className="p-3 pt-2 pb-0">
-              <p className="text-gray-600">
-                Machine Learning in Mobile Apps: Practical Applications
-              </p>
-            </div>
-            <div className="pl-3">
-              <p>@zaka</p>
-            </div>
-          </div>
-        </div>
+      <div className="w-[273px] ml-4 ">
+        <TrendingProjects />
       </div>
     </div>
   );
