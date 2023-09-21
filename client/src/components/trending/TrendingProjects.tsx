e4d7ae3033268ca1e1e5a5bb5626916f2db7e880
ghyorka <ghyorkagospel@gmail.com>
2023-09-22 00:06:39 +0200

    chore(trendingprojects) Minor code adjustment to the trending project component

--- client/src/components/trending/TrendingProjects.tsx
+++ client/src/components/trending/TrendingProjects.tsx
@@ -22,23 +22,21 @@ function TrendingProjects() {
   }, [fetchDispatch]);
 
   return (
-    <div className=" flex flex-col items-start justify-start p-4 ">
-      <div className="w-[280px] h-[400px] border border-gray-400 rounded shadow p-4">
-        <h3 className="text-xl font-semibold mb-4 flex items-center">
-          Trending Projects
-          <span className="flex flex-row ml-2">
-            <img className="w-6 h-6" src={fire} alt="fire" />
-            <img className="w-6 h-6" src={fire} alt="fire" />
-            <img className="w-6 h-6" src={fire} alt="fire" />
-          </span>
-        </h3>
-        <div className="flex flex-col space-y-2">
-          {topPosts.map((post) => (
-            <div key={post._id}>
-              <TrendingProjectItem post={post} />
-            </div>
-          ))}
-        </div>
+    <div className="bg-white p-4 rounded-lg shadow">
+      <h3 className="text-xl font-semibold mb-4 flex items-center">
+        Trending Topics
+        <span className="flex flex-row ml-2">
+          <img className="w-6 h-6" src={fire} alt="fire" />
+          <img className="w-6 h-6" src={fire} alt="fire" />
+          <img className="w-6 h-6" src={fire} alt="fire" />
+        </span>
+      </h3>
+      <div className="flex flex-col space-y-2">
+        {topPosts.map((post) => (
+          <div key={post._id}>
+            <TrendingProjectItem post={post} />
+          </div>
+        ))}
       </div>
     </div>
   );
