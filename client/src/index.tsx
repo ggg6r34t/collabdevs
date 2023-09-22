4a2dd3ec1383fc5470e2010a6e49caacdec5cb2c
ghyorka <ghyorkagospel@gmail.com>
2023-09-22 19:11:48 +0200

    chore(index) Minor code adjustment

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
-import App from "./App";
-
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
-import { store } from "../src/redux/store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "react-confirm-alert/src/react-confirm-alert.css";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "../src/redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
