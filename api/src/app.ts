a7bbfc4b72bb1c2d0780703d63fad1c04837415a
ghyorka <ghyorkagospel@gmail.com>
2023-10-03 19:31:31 +0200

    refactor(app) Refactor CORS code logic in app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -52,8 +52,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://bucolic-brioche-12d61d.netlify.app/"], // client's domain
-  methods: "GET,PUT,PATCH,POST,DELETE",
+  methods: "GET,PUT,POST,DELETE",
   credentials: true,
   exposedHeaders: ["Authorization", "X-Custom-Header"],
 };
