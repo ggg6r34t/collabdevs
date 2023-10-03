98ba84051a93650bf1539e9124b7d03ba94f4a2e
ghyorka <ghyorkagospel@gmail.com>
2023-10-04 00:37:05 +0200

    chore(useSignIn): Minor code adjustment

--- client/src/hooks/useSignIn.ts
+++ client/src/hooks/useSignIn.ts
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 import axios from "../api/axiosConfig";
 import { useSetUserSession } from "./useSetUserSession";
-import { LoginCredentials } from "../type/types";
+import { LoginCredentials, User } from "../type/types";
 
 export const useSignIn = () => {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ export const useSignIn = () => {
 
       if (response.status === 200) {
         // store the userData and userToken securely in local storage
-        const userData = response.data.userData; // from data object. get and assign the token
+        const userData: User = response.data.userData; // from data object. get and assign the token
 
         // save userData to redux
         setUserSession(userData);
