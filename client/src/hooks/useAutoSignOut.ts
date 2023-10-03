b346626fea2d2d75c99b76c020b8f127a1c6ffcd
ghyorka <ghyorkagospel@gmail.com>
2023-10-03 18:22:12 +0200

    feat(auth): Implement auto sign-in and sign-out functionality
    - Added useAutoSignIn hook for automatically signing in users on page load.
    - Created useAutoSignOut hook to handle automatic sign-out when tokens are expired.

---
+++ client/src/hooks/useAutoSignOut.ts
@@ -0,0 +1,107 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Cookies from "js-cookie";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+import { BASE_URL } from "../api/api";
+import { RootState } from "../redux/store";
+import { useClearUserSession } from "./useClearUserSession";
+
+export const useAutoSignOut = () => {
+  const userInformation = useSelector(
+    (state: RootState) => state.user.userInformation
+  );
+
+  const navigate = useNavigate();
+
+  // useClearUserSession hook to get the clearUserSession function
+  const { clearUserSession } = useClearUserSession();
+
+  // Function to decode a JWT token and retrieve the expiration time
+  const getTokenExpiration = (token: string) => {
+    try {
+      const decodedToken = jwt.decode(token) as JwtPayload; // Use type assertion
+
+      if (decodedToken && typeof decodedToken.exp === "number") {
+        return decodedToken.exp; // This should contain the token expiration time in Unix timestamp format
+      }
+    } catch (error) {
+      console.error("Error decoding token:", error);
+    }
+
+    return null;
+  };
+
+  const autoSignOut = async () => {
+    try {
+      const token = userInformation?.token;
+
+      if (token) {
+        const tokenExpiration = getTokenExpiration(token); // Get the token expiration
+
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        if (tokenExpiration && tokenExpiration < currentTime) {
+          console.error("Token is expired.");
+
+          // clear user session (token, cookies, etc.)
+          clearUserSession();
+
+          navigate("/signin");
+          return; // exit the function
+        }
+
+        // check if the session cookie has expired
+        const isSessionCookieExpired = () => {
+          const sessionCookie = Cookies.get("session"); // session cookie name
+          return !sessionCookie; // check if the cookie is not present
+        };
+
+        // perform the session cookie check
+        if (isSessionCookieExpired()) {
+          console.error("Session cookie is expired.");
+
+          // clear user session (token, cookies, etc.)
+          clearUserSession();
+
+          navigate("/signin");
+          return; // exit the function
+        }
+
+        // token is expired, proceed with sign-out
+        const response = await axios.post(
+          `${BASE_URL}/api/v1/users/autoSignout`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          // clear user session (token, cookies, etc.)
+          clearUserSession();
+
+          navigate("/signin");
+        } else {
+          console.error("Sign out failed");
+        }
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        console.error("Unauthorized: Token is invalid or expired.");
+
+        // clear user session (token, cookies, etc.)
+        clearUserSession();
+
+        navigate("/signin");
+      } else {
+        console.error("Error during sign out:", error);
+      }
+    }
+  };
+
+  return { autoSignOut };
+};
