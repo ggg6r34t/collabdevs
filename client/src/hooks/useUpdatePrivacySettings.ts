2f02d1ec63473f333a8e139991249d55349db204
ghyorka <ghyorkagospel@gmail.com>
2023-10-03 23:06:48 +0200

    feat(hooks): Add useUpdatePrivacySettings custom hook
    This hook offers the ability to update user privacy settings, including profile visibility and email visibility, through API requests.

---
+++ client/src/hooks/useUpdatePrivacySettings.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { BASE_URL } from "../api/api";
+
+const useUpdatePrivacySettings = () => {
+  const updatePrivacySettings = async (
+    profileVisibility: boolean,
+    emailVisibility: boolean
+  ) => {
+    try {
+      const token = localStorage.getItem("userToken");
+      const response = await axios.post(
+        `${BASE_URL}/api/v1/users/update-privacy-settings`,
+        { profileVisibility, emailVisibility },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        // privacy settings updated
+      } else {
+        console.error("Update privacy settings failed");
+      }
+    } catch (error) {
+      console.error("Error updating privacy settings:", error);
+    }
+  };
+
+  return {
+    updatePrivacySettings,
+  };
+};
+
+export default useUpdatePrivacySettings;
