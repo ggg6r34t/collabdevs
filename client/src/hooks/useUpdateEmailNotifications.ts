b9c7f6a2d6398e5ed047b5ba78b0ab172c84a0c4
ghyorka <ghyorkagospel@gmail.com>
2023-10-03 23:06:23 +0200

    feat(hooks): Add useUpdateEmailNotifications custom hook
    This hook allows users to update their email notification preferences via API calls, making it easy to manage their settings.

---
+++ client/src/hooks/useUpdateEmailNotifications.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { BASE_URL } from "../api/api";
+
+const useUpdateEmailNotifications = () => {
+  const updateEmailNotifications = async (preference: string) => {
+    try {
+      const token = localStorage.getItem("userToken");
+      const response = await axios.post(
+        `${BASE_URL}/api/v1/users/update-email-notifications`,
+        { preference },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        // email notification preference updated
+      } else {
+        console.error("Update email notifications failed");
+      }
+    } catch (error) {
+      console.error("Error updating email notifications:", error);
+    }
+  };
+
+  return {
+    updateEmailNotifications,
+  };
+};
+
+export default useUpdateEmailNotifications;
