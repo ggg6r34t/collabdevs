3a988d85b19201e97e7f4c4df66d1487192a19f9
ghyorka <ghyorkagospel@gmail.com>
2023-10-09 12:32:39 +0200

    feat: Add loading state to userPosts tab on the profile page
    - Implemented a loading state for the userPosts tab on the profile page to enhance user experience during data loading.

--- client/src/components/user/profile/UserPosts.tsx
+++ client/src/components/user/profile/UserPosts.tsx
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-
 import { RootState } from "../../../redux/store";
 import PostItem from "../../community/PostItem";
 
@@ -8,31 +7,45 @@ function UserPosts() {
     (state: RootState) => state.posts.postsByUserId
   );
 
+  if (!userPosts) {
+    // loading state
+    return (
+      <div className="p-6">
+        <h2 className="text-lg font-semibold mb-4">Your Posts</h2>
+        <div className="flex items-center justify-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
+        </div>
+      </div>
+    );
+  }
+
+  if (userPosts.length === 0) {
+    // no posts available
+    return (
+      <div className="p-6">
+        <h2 className="text-lg font-semibold mb-4">My Posts</h2>
+        <div className="flex items-center justify-center p-8">
+          <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+            <p className="text-xl font-semibold text-gray-600">
+              No posts available.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  // render user posts
   return (
     <div className="p-6">
-      <h2 className="text-lg font-semibold mb-4">Your Posts</h2>
-      {userPosts ? (
-        userPosts.length === 0 ? (
-          <div className="flex items-center justify-center p-8">
-            <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-              <p className="text-xl font-semibold text-gray-600">
-                No posts available.
-              </p>
-            </div>
-          </div>
-        ) : (
-          userPosts.map((post) => (
-            <div
-              key={post._id}
-              className="bg-white dark:bg-slate-800 border border-gray-200 p-4 rounded-lg shadow mb-4"
-            >
-              <PostItem post={post} />
-            </div>
-          ))
-        )
-      ) : (
-        <p className="text-gray-500">Loading posts...</p>
-      )}
+      {userPosts.map((post) => (
+        <div
+          key={post._id}
+          className="bg-white dark:bg-slate-800 border border-gray-600 p-4 rounded-lg shadow mb-4"
+        >
+          <PostItem post={post} />
+        </div>
+      ))}
     </div>
   );
 }
